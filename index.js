67149a7ac9e5095228065d2d49d62974ddb6e717
stevenr <steven@ph0bos.io>
2018-02-11 10:15:02 +0000

    Rename implementations

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const ContainerDNSClient = require('./lib/implementations/container-dns-request');
-const HostPortClient     = require('./lib/implementations/host-port-request');
+const ContainerDNSClient = require('./lib/implementations/container-dns-request-client');
+const HostPortClient     = require('./lib/implementations/host-port-request-client');
 
 module.exports = { ContainerDNSClient, HostPortClient };
